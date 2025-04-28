f1d96b0421671d568c35d08f3c1b1d63c6ae5db4
g-jacks <gsirijack@gmail.com>
2025-04-28 21:17:45 +0530

    updated databases

--- taskly/base/admin.py
+++ taskly/base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import sample
+from .models import sample, task
 
 # Register your models here.
-admin.site.register(sample)
+models_list = [sample, task]
+for i in models_list:
+    admin.site.register(i)
